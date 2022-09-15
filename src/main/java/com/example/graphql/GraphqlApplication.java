4ef2d330756ce38bd07054b5ac10605867da9d56
DigvijayBhakuni <digvijay.bhakuni@tetruscorp.com>
2022-09-15 12:01:42 +0530

    update for k8s and loading data

--- src/main/java/com/example/graphql/GraphqlApplication.java
+++ src/main/java/com/example/graphql/GraphqlApplication.java
@@ -20,19 +20,26 @@ public class GraphqlApplication {
 	}
 
 
-//	@Bean
+	@Bean
 	public CommandLineRunner runner(PostRepo postRepo, AuthorRepo authorRepo) {
 		return args -> {
 
-			Author joe = authorRepo.save(new Author(null, "Joe"));
-			Author jack = authorRepo.save(new Author(null, "Jack"));
+			long count = authorRepo.count();
+			System.out.println("count = " + count);
+			if(count < 1) {
+				Author joe = authorRepo.save(new Author(null, "Joe"));
+				Author jack = authorRepo.save(new Author(null, "Jack"));
 
-			Date now = new Date();
-			Post post1 = postRepo.save(new Post(null, "Best Programming Language", "1 Java, 2 Python, 3 Typescript", joe, List.of("java", "python", "typescript")));
-			Post post2 = postRepo.save(new Post(null, "Docker Alternative", "1 podman, 2 kubernetes, 3 cri-o", joe, List.of("docker", "podman", "k8s", "cri-o")));
-			Post post3 = postRepo.save(new Post(null, "Best Cloud Provider", "1 AWS, 2 GCP, 3 Azure", jack, List.of("aws", "gcp", "azure")));
+				Date now = new Date();
+				long postCount = postRepo.count();
+				System.out.println("postCount = " + postCount);
+				if(postCount < 1) {
+					Post post1 = postRepo.save(new Post(null, "Best Programming Language", "1 Java, 2 Python, 3 Typescript", joe, List.of("java", "python", "typescript")));
+					Post post2 = postRepo.save(new Post(null, "Docker Alternative", "1 podman, 2 kubernetes, 3 cri-o", joe, List.of("docker", "podman", "k8s", "cri-o")));
+					Post post3 = postRepo.save(new Post(null, "Best Cloud Provider", "1 AWS, 2 GCP, 3 Azure", jack, List.of("aws", "gcp", "azure")));
+				}
 //			Post post1 = postRepo.save(new Post(null, "Best Programming Language", "1 Java, 2 Python, 3 Typescript", joe, List.of("java", "python", "typescript")));
-
+			}
 		};
 	}
 
